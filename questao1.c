e2957c964cf4a918bb64b2ee87264ca50f111bfb
CodeRodrigo <rodri.linhares93@gmail.com>
2026-08-25 17:10:27 -0400

    Questao_01

--- questao1.c
+++ questao1.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <locale.h>
-    
+
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
 
-    printf("Olá mundinho cheio de delicinhas");
+    int numero;
+
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+
+    if (numero >= 0)
+    {
+        printf("numero é: %d = POSITIVO \n", numero);
+    }
+    else
+    {
+        printf("numero é: %d = NEGATIVO \n", numero);
+    }
+    return 0;
 }
