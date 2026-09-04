236691242d06359f223d335c5fb60d7c1866b76e
CodeRodrigo <rodri.linhares93@gmail.com>
2026-09-03 22:15:46 -0400

    praticando.att

--- Exercicios_praticos/clientedesconto.c
+++ Exercicios_praticos/clientedesconto.c
@@ -8,6 +8,7 @@ int main()
     printf("Digite o valor da compra: ");
     scanf("%lf", &valor);
 
+    // 3 jeitos de tirar porcentagem, mostrando o percentual ou sem.
     if (valor >= 1000)
     {
         percentual = 20;
@@ -19,10 +20,12 @@ int main()
         desconto = valor * 0.08;
     }
 
+    // percentual = desconto / valor * 100;  2ª jeito
+    // desconto = percentual * valor / 100;  3ª jeito
     valorTotal = valor - desconto;
 
     printf("\nO valor original da compra: %.2lf R$", valor);
-    printf("\nO percentual do desoconto aplicado:%.0lf%% ", percentual);
+    // printf("\nO percentual do desoconto aplicado:%.0lf%% ", percentual);
     printf("\nO valor do desconto: %.2lf R$", desconto);
     printf("\nO valor final da compra: %.2lf R$", valorTotal);
     return 0;
