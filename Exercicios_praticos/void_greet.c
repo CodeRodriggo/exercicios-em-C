70154f1cd90e2248ecb576b84cb0770b6cd8dff6
CodeRodrigo <rodri.linhares93@gmail.com>
2026-09-15 23:08:12 -0400

    void_greet

---
+++ Exercicios_praticos/void_greet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void greet()
+{
+    printf("Life is getting better, i hope so...\n");
+}
+
+int main()
+{
+
+    greet();
+    greet();
+    greet();
+    greet();
+
+    return 0;
+}
